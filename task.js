435b75013467243f39a7c2700b87ef0cd67d0f73
Max <chierov@yandex.ru>
2024-12-18 12:58:51 +0300

    second commit

--- task.js
+++ task.js
@@ -1,22 +1,15 @@
 let cases = document.querySelectorAll('span.rotator__case');
-
 let currentCase = 0;
 
 setInterval(() => {
 
-    if(currentCase >= 0){
         cases[currentCase].classList.remove('rotator__case_active');
-        if(currentCase == cases.length - 1){
-            cases[6].classList.remove('rotator__case_active');
+        if(currentCase < cases.length -1){
+        currentCase += 1;
+        cases[currentCase].classList.add('rotator__case_active');
+        }else{
             currentCase = 0;
             cases[currentCase].classList.add('rotator__case_active');
-        }else{
-            currentCase += 1;
-            cases[currentCase].classList.add('rotator__case_active');
-            let current = currentCase - 1;
-            cases[current].classList.remove('rotator__case_active')
         }
-    }
-
-
-}, 2000)
+    
+}, 1000)
